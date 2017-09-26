2431dadcad8b951b1b5bc41cc4809623cb3c0001
BurkayDurdu <burkaydurdu@outlook.com>
2017-09-27 02:00:55 +0300

    create project

---
+++ imports/startup/client/index.js
@@ -0,0 +1,11 @@
+//layouts
+import '/imports/ui/layouts/'
+import '/imports/ui/components/navBar/'
+//pages
+import '/imports/ui/pages/login/'
+import '/imports/ui/pages/notFound/'
+
+//Router
+import './routes.js'
+//Global
+import './utils.js'
