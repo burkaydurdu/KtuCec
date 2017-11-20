eed143ea89e7f78cdd36d15e1693b8312d78e0f7
burkaydurdu <burkaydurdu@burkaydurdus-MacBook-Air.local>
2017-11-21 02:10:01 +0300

    modified login and register page

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -1,54 +1,46 @@
 Meteor.methods({
-    'user.create' : function (data) {
-        try {
-            user_id = Accounts.createUser({
-                username: data.username,
-                email: data.email,
-                password: data.password,
-                profile: {
-                    name: data.name,
-                    surname: data.surname,
-                    schoolNumber: data.schoolNumber,
-                    startYear: data.startYear
-                }
-            });
-            Roles.addUsersToRoles(user_id, ['user']);
-            Accounts.sendVerificationEmail(user_id);
-        } catch (e) {
-            throw new Meteor.Error(100, "can not create");
+  'user.create': function(data) {
+    try {
+      user_id = Accounts.createUser({
+        username: data.username,
+        email: data.schoolNumber + "@ogr.ktu.edu.tr",
+        password: data.password,
+        profile: {
+          name: data.name,
+          surname: data.surname,
+          schoolNumber: data.schoolNumber,
+          startYear: data.startYear
         }
-    },
-    'user.verified.email': function (email_or_name) {
-        try {
-            user = Accounts.findUserByEmail(email_or_name);
-            user = user !== undefined ? user : Accounts.findUserByUsername(email_or_name);
-            if (user !== undefined) {
-                return {
-                    user: true,
-                    verified: user.emails[0].verified
-                }
-            } else {
-                return {
-                    user: false,
-                    verified: false
-                }
-            }
-        } catch (e) {
-            throw new Meteor.Error(100, 'Error');
-        }
-    },
-    'profile.picture.add': function (pictureId) {
-        try {
-            Meteor.users.update(Meteor.userId(), {
-                $addToSet: {
-                    'profile.profilePictureId' : pictureId
-                }
-            });
-        }  catch (e) {
-            throw new Meteor.Error(101, "can not add");
-        }
-    },
-    'role.create' : function (role) {
-        Roles.addUsersToRoles(Meteor.users, [role]);
+      });
+      Roles.addUsersToRoles(user_id, ['user']);
+      Accounts.sendVerificationEmail(user_id);
+    } catch (e) {
+      throw new Meteor.Error(e.error, e.reason);
     }
+  },
+  'user.verified.email': function(email_or_name) {
+    try {
+      user = Accounts.findUserByEmail(email_or_name);
+      user = user !== undefined ? user : Accounts.findUserByUsername(email_or_name);
+      return user !== undefined ? {
+        user: true,
+        verified: user.emails[0].verified
+      } : {
+        user: false
+      }
+    } catch (e) {
+      throw new Meteor.Error(100, 'Error');
+    }
+  },
+  'profile.picture.add': function(pictureId) {
+    try {
+      Meteor.users.update(Meteor.userId(), {
+        $addToSet: {
+          'profile.profilePictureId': pictureId
+        }
+      });
+    } catch (e) {
+      throw new Meteor.Error(101, "can not add");
+    }
+  }
 });
--- imports/ui/pages/register/index.js
+++ imports/ui/pages/register/index.js
@@ -2,35 +2,28 @@ import './index.html'
 import './index.css'
 
 Template.register.events({
-    'submit form#registerForm' : function (event) {
-        event.preventDefault();
-        data = {
-            username : event.target.username.value,
-            name : event.target.name.value,
-            surname : event.target.surname.value,
-            password : event.target.password.value,
-            passwordConfirm : event.target.passwordConfirm.value,
-            schoolNumber : event.target.schoolNumber.value,
-            startYear : event.target.startYear.value,
-            email : event.target.email.value,
-        };
-
-        emailArray = data.email.split('@');
-        if (emailArray[1] === 'ogr.ktu.edu.tr') {
-            if (data.password === data.passwordConfirm) {
-                Meteor.call('user.create', data, function (err, res) {
-                    if (!err) {
-                        Materialize.toast("Kayit islemi basarili. Lutfen mail adresine gelen mesaji onaylayin. Daha sonra giris yapabilirsiniz.", 2500, "green white-text");
-                        FlowRouter.go('/login');
-                    } else {
-                        // TODO buraya mail username hatalari yada diger hatalari belirleyip mesaj cikarilicaktir.
-                    }
-                })
-            } else {
-                Materialize.toast("Check Password", 2500, "red white-text");
-            }
+  'submit form#registerForm': function(event) {
+    event.preventDefault();
+    data = {
+      username: event.target.username.value,
+      name: event.target.name.value,
+      surname: event.target.surname.value,
+      password: event.target.password.value,
+      passwordConfirm: event.target.passwordConfirm.value,
+      schoolNumber: event.target.schoolNumber.value,
+      startYear: event.target.startYear.value,
+    };
+    if (data.password === data.passwordConfirm) {
+      Meteor.call('user.create', data, function(err, res) {
+        if (!err) {
+          Materialize.toast("Kayit islemi basarili. Lutfen mail adresine gelen mesaji onaylayin. Daha sonra giris yapabilirsiniz.", 3500, "green white-text");
+          FlowRouter.go('/login');
         } else {
-            Materialize.toast("Please, enter the school email adress", 2500, "red white-text");
+          Materialize.toast(err.reason, 2500, "red white-text");
         }
+      })
+    } else {
+      Materialize.toast("Sifrenizi kontrol edin!!", 2500, "red white-text");
     }
+  }
 });
